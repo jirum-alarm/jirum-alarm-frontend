b68b98633837a18620904e80ec67147198f36421
seonkyo <tjs0466@naver.com>
2023-06-18 23:33:24 +0900

    misc: modify subscription endpoint

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -2,4 +2,4 @@ export const GRAPHQL_ENDPOINT =
   process.env.GRAPHQL_ENDPOINT || "https://jirum-api.kyojs.com/graphql";
 export const GRAPHQL_SUBSCRIPTIONS_ENDPOINT =
   process.env.SUBSCRIPTIONS_GRAPHQL_ENDPOINT ||
-  "ws://jirum-api.kyojs.com/graphql";
+  "wss://jirum-api.kyojs.com/graphql";
