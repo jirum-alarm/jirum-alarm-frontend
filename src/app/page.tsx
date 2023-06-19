63a8e577323f1ea439b965206c1534ed624ee25d
seonkyo <tjs0466@naver.com>
2023-06-20 01:24:49 +0900

    디자인 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getClient } from "../lib/client";
 
 const query = gql`
   query {
-    products(limit: 10) {
+    products(limit: 100) {
       id
       title
       url
@@ -21,17 +21,23 @@ export default async function Home() {
   });
 
   return (
-    <main className="flex min-w-min max-w-screen-md mx-auto">
-      <div className="item-center grid grid-cols-2 sm:grid-cols-4 gap-4 m-10">
-        {data.data.products.map((product) => (
-          <div key={product.id} className="border border-black">
-            <div className="">
-              <a href={product.url}>
-                <h3>{product.title}</h3>
-              </a>
-            </div>
+    <main>
+      <div className="min-w-min max-w-screen-md mx-auto">
+        <div className="mt-10">
+          <h1 className="text-center text-4xl font-medium">지름알림🔔</h1>
+        </div>
+
+        <div className="flex ">
+          <div className="item-center grid grid-cols-2 sm:grid-cols-4 gap-4 m-10">
+            {data.data.products.map((product) => (
+              <div key={product.id} className="border border-black h-full py-2">
+                <a href={product.url} target="_blank">
+                  <h3 className="h-full">{product.title}</h3>
+                </a>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    minHeight: {
+      0: "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+    },
   },
   variants: {
     extend: {},
