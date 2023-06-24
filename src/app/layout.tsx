a2b4ed9ec62859ce1c8b85e67bcc1c658851cea2
seonkyo <tjs0466@naver.com>
2023-06-25 04:53:43 +0900

    og 정보 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,12 @@ import { ApolloWrapper } from "../lib/apollo-wrapper";
 export const metadata = {
   title: "지름알림🔔",
   description: "지름 정보를 알려드려요!",
+  openGraph: {
+    title: "지름알림🔔: 핫딜 정보 보아보기",
+    author: "지름알림",
+    description: "핫딜 정보를 알려드려요!",
+    images: "./opengraph-image.png",
+  },
 };
 
 export default function RootLayout({
