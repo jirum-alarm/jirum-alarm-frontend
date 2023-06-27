d1b016c511577a0472a2b06b11f6d57b90a6fd55
seonkyo <tjs0466@naver.com>
2023-06-28 02:45:32 +0900

    modify graphql endpoint

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -1,5 +1,5 @@
 export const GRAPHQL_ENDPOINT =
-  process.env.GRAPHQL_ENDPOINT || "http://jirum-api.kyojs.com/graphql";
+  process.env.GRAPHQL_ENDPOINT || "https://jirum-api.kyojs.com/graphql";
 export const GRAPHQL_SUBSCRIPTIONS_ENDPOINT =
   process.env.SUBSCRIPTIONS_GRAPHQL_ENDPOINT ||
   "wss://jirum-api.kyojs.com/graphql";
