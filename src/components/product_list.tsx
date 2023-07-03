e7f1fed1d4b41b6c4447b23bc8132331842a1230
seonkyo <tjs0466@naver.com>
2023-07-04 00:45:38 +0900

    탭 적용

--- src/components/product_list.tsx
+++ src/components/product_list.tsx
@@ -1,25 +1,25 @@
-'use client';
+"use client";
 
-import 'react-tabs/style/react-tabs.css';
-import '../style/React_tabs.css';
-import { useSuspenseQuery } from '@apollo/experimental-nextjs-app-support/ssr';
-import { useCallback, useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
-import SwipeableViews from 'react-swipeable-views';
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+import { useCallback, useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import SwipeableViews from "react-swipeable-views";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import "../style/React_tabs.css";
 
-import { QueryProducts } from '../graphql';
-import { IProduct, IProductOutput } from '../graphql/interface';
-import Product from './product';
-import { QueryCategories } from '../graphql/category';
-import { ICategoryOutput } from '../graphql/interface/category';
+import { QueryProducts } from "../graphql";
+import { QueryCategories } from "../graphql/category";
+import { IProduct, IProductOutput } from "../graphql/interface";
+import { ICategoryOutput } from "../graphql/interface/category";
+import Product from "./product";
 
 export default function ProductList() {
   const limit = 20;
   const [isMobile, setIsMobile] = useState(false);
   const [products, setProducts] = useState<IProduct[]>([]);
-  const [searchAfter, setSearchAfter] = useState<string[] | undefined>([]);
   const [hasNext, setHasNext] = useState<boolean>(true);
+  const [previousActiveTab, setPreviousActiveTab] = useState<number>(0);
   const { ref, inView } = useInView({ threshold: 0 });
   const [activeTab, setActiveTab] = useState(0);
 
@@ -27,51 +27,68 @@ export default function ProductList() {
     setActiveTab(index);
   }, []);
 
-  const { data, error, fetchMore } = useSuspenseQuery<IProductOutput>(QueryProducts, {
-    variables: { limit },
-  });
+  const { data, error, fetchMore } = useSuspenseQuery<IProductOutput>(
+    QueryProducts,
+    {
+      variables: { limit },
+    },
+  );
 
-  const { data: categoriesData } = useSuspenseQuery<ICategoryOutput>(QueryCategories);
+  const { data: categoriesData } =
+    useSuspenseQuery<ICategoryOutput>(QueryCategories);
 
   const isMobileDevice = useCallback(() => {
     const userAgent = window.navigator.userAgent;
     const isMobileDevice = Boolean(
-      userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i)
+      userAgent.match(
+        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i,
+      ),
     );
     return isMobileDevice;
   }, []);
 
   const fetch = useCallback(async () => {
-    if (hasNext) {
-      if (data.products.length && products.length === 0) {
-        setSearchAfter(data.products.at(-1)?.searchAfter);
-        setProducts(data.products);
-      }
+    const modifiedTab = previousActiveTab !== activeTab;
+    if (!modifiedTab) {
+      const lastProduct = products.at(-1);
+      const searchAfter = lastProduct?.searchAfter;
 
-      const categoryId = activeTab === 0 ? undefined : +categoriesData.categories[activeTab - 1].id;
+      const categoryId =
+        activeTab === 0
+          ? undefined
+          : +categoriesData.categories[activeTab - 1].id;
 
       const newProducts = await fetchMore({
         variables: { limit, searchAfter, categoryId },
       });
 
-      // if (products.some((product) => product.categoryId !== categoryId)) {
-
-      //   setHasNext(true);
-      //   setProducts(newProducts.data.products);
-      //   const lastProduct = newProducts.data.products.at(-1);
-      //   setSearchAfter(lastProduct?.searchAfter);
-      //   return;
-      // }
-
       setHasNext(newProducts.data.products.length === limit);
       setProducts([...products, ...newProducts.data.products]);
-      const lastProduct1 = newProducts.data.products.at(-1);
-      setSearchAfter(lastProduct1?.searchAfter);
+      return;
     }
+
+    const categoryId =
+      activeTab === 0
+        ? undefined
+        : +categoriesData.categories[activeTab - 1].id;
+
+    const newProducts = await fetchMore({
+      variables: { limit, categoryId },
+    });
+
+    setHasNext(newProducts.data.products.length === limit);
+    setProducts(newProducts.data.products);
   }, [hasNext, inView, activeTab]);
 
   useEffect(() => {
     setIsMobile(isMobileDevice());
+
+    const modifiedTab = previousActiveTab !== activeTab;
+    if (modifiedTab) {
+      fetch();
+      setPreviousActiveTab(() => activeTab);
+      return;
+    }
 
     if (inView && hasNext) {
       fetch();
@@ -85,74 +102,70 @@ export default function ProductList() {
       ) : !data ? (
         <p>로딩중....</p>
       ) : (
-        <div>
-          <div>
-            <Tabs
-              className="react-tabs__tab-list"
-              forceRenderTabPanel={true}
-              selectedIndex={activeTab}
-              onSelect={handleTabChange}
+        <Tabs
+          className="react-tabs__tab-list"
+          forceRenderTabPanel={true}
+          selectedIndex={activeTab}
+          onSelect={handleTabChange}
+        >
+          <TabList>
+            <Tab
+              className="inline-block p-4 text-zinc-400	b-0"
+              id="profile-tab"
+              data-tabs-target="#profile"
+              type="button"
+              role="tab"
+              aria-controls="profile"
+              aria-selected="false"
             >
-              <TabList>
-                <Tab
-                  className="inline-block p-4 text-zinc-400	b-0 "
-                  id="profile-tab"
-                  data-tabs-target="#profile"
-                  type="button"
-                  role="tab"
-                  aria-controls="profile"
-                  aria-selected="false"
-                >
-                  전체
-                </Tab>
+              전체
+            </Tab>
 
-                {categoriesData.categories.map((category) => (
-                  <Tab
-                    key={category.id}
-                    className="inline-block p-4 text-zinc-400 	"
-                    id="profile-tab"
-                    data-tabs-target="#profile"
-                    type="button"
-                    role="tab"
-                    aria-controls="profile"
-                    aria-selected="false"
-                  >
-                    <button>{category.name}</button>
-                  </Tab>
-                ))}
-              </TabList>
-
-              <SwipeableViews
-                index={activeTab}
-                onChangeIndex={handleTabChange}
-                animateTransitions={isMobile}
+            {categoriesData.categories.map((category) => (
+              <Tab
+                key={category.id}
+                className="inline-block p-4 text-zinc-400	b-0"
+                id="profile-tab"
+                data-tabs-target="#profile"
+                type="button"
+                role="tab"
+                aria-controls="profile"
+                aria-selected="false"
               >
-                <TabPanel>
-                  <div className="flex">
-                    <div className="item-center mx-5 grid grid-cols-1 gap-8 sm:grid-cols-2">
-                      {products.map((product) => (
-                        <Product key={product.id} product={product}></Product>
-                      ))}
-                    </div>
+                <button>{category.name}</button>
+              </Tab>
+            ))}
+          </TabList>
+
+          <SwipeableViews
+            index={activeTab}
+            onChangeIndex={handleTabChange}
+            animateTransitions={isMobile}
+          >
+            <TabPanel>
+              <div className="flex">
+                <div className="item-center mx-5 grid grid-cols-1 gap-8 sm:grid-cols-2">
+                  {products.map((product) => (
+                    <Product key={product.id} product={product}></Product>
+                  ))}
+                </div>
+              </div>
+            </TabPanel>
+            {categoriesData.categories.map((category) => (
+              <TabPanel key={category.id}>
+                <div className="flex">
+                  <div className="item-center mx-5 grid grid-cols-1 gap-8 sm:grid-cols-2">
+                    {products.map((product) => (
+                      <Product key={product.id} product={product}></Product>
+                    ))}
                   </div>
-                </TabPanel>
-                {categoriesData.categories.map((category) => (
-                  <TabPanel key={category.id}>
-                    <div className="flex">
-                      <div className="item-center mx-5 grid grid-cols-1 gap-8 sm:grid-cols-2">
-                        {products.map((product) => (
-                          <Product key={product.id} product={product}></Product>
-                        ))}
-                      </div>
-                    </div>
-                  </TabPanel>
-                ))}
-              </SwipeableViews>
-            </Tabs>
-          </div>
-        </div>
+                </div>
+              </TabPanel>
+            ))}
+          </SwipeableViews>
+          <div ref={ref} className="h-48 w-full" />
+        </Tabs>
       )}
-      <div ref={ref} className="h-48 w-full" />
     </main>
   );
 }
