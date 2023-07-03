b31a47d2da755b837a65b5c33a636b9219c7f355
seonkyo <tjs0466@naver.com>
2023-07-04 02:03:24 +0900

    탭 스크롤 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1 " />
       </Head>
-      <div className="font-mono min-w-min max-w-screen-lg mx-auto ">
+      <div className="font-mono max-w-screen-lg mx-auto">
         <div className="p-8 flex justify-center ">
           <div className="flex content-center ">
             <h1 className="text-center flex center text-4xl font-medium">
