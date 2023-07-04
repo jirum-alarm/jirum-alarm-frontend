0a848484226db5a602ce8916562d2db0f3636ac3
seonkyo <tjs0466@naver.com>
2023-07-05 02:49:32 +0900

    search box test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,33 @@ export default async function Home() {
             </div>
           </div>
 
+          <div className="max-w-md mx-auto">
+            <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+              <div className="grid place-items-center h-full w-12 text-gray-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </div>
+
+              <input
+                className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+                type="text"
+                id="search"
+                placeholder="Search something.."
+              />
+            </div>
+          </div>
           <ProductList />
         </div>
       </div>
