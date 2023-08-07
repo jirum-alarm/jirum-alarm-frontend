cec1ee4ccc26c5e90c156eace0a54e72b0e72c0e
tjsry0466 <tjs0466@naver.com>
2023-08-08 03:01:30 +0900

    wip

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -33,44 +33,11 @@ export default async function Home() {
             </div>
             <div className="p-2">
               <div className="grid gap-2">
-                <button
-                  type="button"
-                  className="h-12 duration-200 border border-gray-200 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold relative"
-                >
-                  <svg
-                    width="32"
-                    height="32"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 64 64"
-                    preserveAspectRatio="none"
-                    className="absolute top-2 left-2"
+                <a href="https://jirum-api.kyojs.com/login/kakao">
+                  <button
+                    type="button"
+                    className="h-12 transition duration-200 border border-gray-200 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold relative"
                   >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M29.85 0h4.3c7.517 0 12.376 1.202 16.598 3.46 4.221 2.258 7.534 5.57 9.792 9.792C62.798 17.474 64 22.332 64 29.85v4.3c0 7.517-1.202 12.376-3.46 16.598-2.258 4.221-5.57 7.534-9.792 9.792C46.526 62.798 41.668 64 34.15 64h-4.3c-7.517 0-12.376-1.202-16.598-3.46-4.221-2.258-7.534-5.57-9.792-9.792C1.202 46.526 0 41.668 0 34.15v-4.3c0-7.517 1.202-12.376 3.46-16.598 2.258-4.221 5.57-7.534 9.792-9.792C17.474 1.202 22.332 0 29.85 0z"
-                      fill="#FFE900"
-                    ></path>
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M32 15c-9.941 0-18 6.455-18 14.418 0 5.149 3.37 9.666 8.437 12.217-.276.966-1.772 6.214-1.831 6.627 0 0-.036.31.161.428.198.118.43.026.43.026.567-.08 6.567-4.363 7.606-5.106a22.45 22.45 0 003.197.227c9.941 0 18-6.456 18-14.419S41.941 15 32 15z"
-                      fill="#000"
-                    ></path>
-                    <path
-                      opacity=".1"
-                      d="M29.85.5h4.3c7.458 0 12.23 1.192 16.362 3.4 4.134 2.212 7.376 5.454 9.587 9.588C62.31 17.62 63.5 22.392 63.5 29.85v4.3c0 7.458-1.192 12.23-3.4 16.362-2.212 4.134-5.454 7.376-9.588 9.587C46.38 62.31 41.608 63.5 34.15 63.5h-4.3c-7.458 0-12.23-1.192-16.362-3.4-4.134-2.212-7.376-5.454-9.587-9.588C1.69 46.38.5 41.608.5 34.15v-4.3c0-7.458 1.192-12.23 3.4-16.362 2.212-4.134 5.454-7.376 9.588-9.587C17.62 1.69 22.392.5 29.85.5z"
-                      stroke="#EFEFEF"
-                    ></path>
-                  </svg>
-                  카카오톡으로 로그인
-                </button>
-                <button
-                  type="button"
-                  className="h-12 transition duration-200 border border-gray-200 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold relative"
-                >
-                  <span>
                     <svg
                       width="32"
                       height="32"
@@ -84,21 +51,56 @@ export default async function Home() {
                         fill-rule="evenodd"
                         clip-rule="evenodd"
                         d="M29.85 0h4.3c7.517 0 12.376 1.202 16.598 3.46 4.221 2.258 7.534 5.57 9.792 9.792C62.798 17.474 64 22.332 64 29.85v4.3c0 7.517-1.202 12.376-3.46 16.598-2.258 4.221-5.57 7.534-9.792 9.792C46.526 62.798 41.668 64 34.15 64h-4.3c-7.517 0-12.376-1.202-16.598-3.46-4.221-2.258-7.534-5.57-9.792-9.792C1.202 46.526 0 41.668 0 34.15v-4.3c0-7.517 1.202-12.376 3.46-16.598 2.258-4.221 5.57-7.534 9.792-9.792C17.474 1.202 22.332 0 29.85 0z"
-                        fill="#00B600"
+                        fill="#FFE900"
                       ></path>
                       <path
                         fill-rule="evenodd"
                         clip-rule="evenodd"
-                        d="M36.45 18v13.658L27.584 18H18v27.07h9.552V31.412l8.866 13.658H46V18h-9.55z"
-                        fill="#fff"
+                        d="M32 15c-9.941 0-18 6.455-18 14.418 0 5.149 3.37 9.666 8.437 12.217-.276.966-1.772 6.214-1.831 6.627 0 0-.036.31.161.428.198.118.43.026.43.026.567-.08 6.567-4.363 7.606-5.106a22.45 22.45 0 003.197.227c9.941 0 18-6.456 18-14.419S41.941 15 32 15z"
+                        fill="#000"
+                      ></path>
+                      <path
+                        opacity=".1"
+                        d="M29.85.5h4.3c7.458 0 12.23 1.192 16.362 3.4 4.134 2.212 7.376 5.454 9.587 9.588C62.31 17.62 63.5 22.392 63.5 29.85v4.3c0 7.458-1.192 12.23-3.4 16.362-2.212 4.134-5.454 7.376-9.588 9.587C46.38 62.31 41.608 63.5 34.15 63.5h-4.3c-7.458 0-12.23-1.192-16.362-3.4-4.134-2.212-7.376-5.454-9.587-9.588C1.69 46.38.5 41.608.5 34.15v-4.3c0-7.458 1.192-12.23 3.4-16.362 2.212-4.134 5.454-7.376 9.588-9.587C17.62 1.69 22.392.5 29.85.5z"
+                        stroke="#EFEFEF"
                       ></path>
                     </svg>
-                  </span>
-                  네이버로 로그인
-                </button>
-                <a
-                  href={`https://kauth.kakao.com/oauth/authorize?client_id=4807ed026ae596e8ae795dfb4c8e4684&redirect_uri=https://jirum-api.kyojs.com/login/kakao&response_type=code`}
-                >
+                    카카오톡으로 로그인
+                  </button>
+                </a>
+                <a href="https://jirum-api.kyojs.com/login/naver">
+                  <button
+                    type="button"
+                    className="h-12 transition duration-200 border border-gray-200 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold relative"
+                  >
+                    <span>
+                      <svg
+                        width="32"
+                        height="32"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 64 64"
+                        preserveAspectRatio="none"
+                        className="absolute top-2 left-2"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M29.85 0h4.3c7.517 0 12.376 1.202 16.598 3.46 4.221 2.258 7.534 5.57 9.792 9.792C62.798 17.474 64 22.332 64 29.85v4.3c0 7.517-1.202 12.376-3.46 16.598-2.258 4.221-5.57 7.534-9.792 9.792C46.526 62.798 41.668 64 34.15 64h-4.3c-7.517 0-12.376-1.202-16.598-3.46-4.221-2.258-7.534-5.57-9.792-9.792C1.202 46.526 0 41.668 0 34.15v-4.3c0-7.517 1.202-12.376 3.46-16.598 2.258-4.221 5.57-7.534 9.792-9.792C17.474 1.202 22.332 0 29.85 0z"
+                          fill="#00B600"
+                        ></path>
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M36.45 18v13.658L27.584 18H18v27.07h9.552V31.412l8.866 13.658H46V18h-9.55z"
+                          fill="#fff"
+                        ></path>
+                      </svg>
+                    </span>
+                    네이버로 로그인
+                  </button>
+                </a>
+                <a href="https://jirum-api.kyojs.com/login/google">
                   <button
                     type="button"
                     className="h-12 relative transition duration-200 border border-gray-200 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
--- src/app/oauth/login/success/page.tsx
+++ src/app/oauth/login/success/page.tsx
@@ -1,3 +1,12 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function OauthLoginSuccess() {
-  return "hello";
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace(`/`);
+  }, []);
+
+  return <>login success</>;
 }
