c79c3e1417e2be93f333f3119891cace3e3cfbcd
tjsry0466 <tjs0466@naver.com>
2023-08-08 03:13:08 +0900

    wip

--- src/app/oauth/login/success/page.tsx
+++ src/app/oauth/login/success/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function OauthLoginSuccess() {
