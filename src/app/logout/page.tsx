03b5247b421622ad4a1e9bcf7d0702208c43868f
tjsry0466 <tjs0466@naver.com>
2023-08-15 19:38:43 +0900

    wip

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -2,15 +2,19 @@
 
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
+import { useSetRecoilState } from 'recoil'
+import { userState } from '../../state/user'
 import { StorageTokenKey } from '../../type/enum/auth'
 
 export default function Logout() {
   const router = useRouter()
+  const setUser = useSetRecoilState(userState)
 
   useEffect(() => {
     localStorage.removeItem(StorageTokenKey.ACCESS_TOKEN)
     localStorage.removeItem(StorageTokenKey.REFRESH_TOKEN)
     localStorage.removeItem('me')
+    setUser(null)
 
     router.push('/')
     return
