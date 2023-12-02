860d288c29721c8e2b6a114819a8b6e4817aa538
yooyeonhee <devyh0629@gmail.com>
2023-12-02 13:44:12 +0900

    Fix: 스와이프 영역 확대

--- src/components/main/productList/ProductList.tsx
+++ src/components/main/productList/ProductList.tsx
@@ -16,6 +16,7 @@ import { Tab, TabList, TabPanel, Tabs } from 'react-tabs'
 import 'react-tabs/style/react-tabs.css'
 import '../../../style/React_tabs.css'
 import ProductNotFound from './ProductNotFound'
+import ProductLoading from './ProductLoading'
 
 const ProductCard = dynamic(() => import('./ProductCard'), { ssr: false })
 const ProductList = () => {
@@ -27,15 +28,23 @@ const ProductList = () => {
   const [keyword, setKeyword] = useState<string>('')
   const [activeTab, setActiveTab] = useState(0)
   const [hasNextData, setHasNextData] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
   const { isMobile } = useDevice()
+  const categoryParam = searchParams.get('categoryId')
+  const keywordParam = searchParams.get('keyword')
 
   const { data: categoriesData } = useSuspenseQuery<ICategoryOutput>(QueryCategories)
+
   const {
     data: { products },
     refetch,
     fetchMore,
   } = useSuspenseQuery<IProductOutput>(QueryProducts, {
-    variables: { limit },
+    variables: {
+      limit,
+      keyword: keywordParam || undefined,
+      categoryId: categoryParam ? Number(categoryParam) : undefined,
+    },
   })
 
   const { ref } = useInView({
@@ -49,6 +58,7 @@ const ProductList = () => {
   const handleTabChange = useCallback((index: number) => {
     setActiveTab(index)
   }, [])
+
   const keywordHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputData(event.target.value)
   }
@@ -92,17 +102,21 @@ const ProductList = () => {
   useEffect(() => {
     const categoryParam = searchParams.get('categoryId')
     const keywordParam = searchParams.get('keyword')
-    if (categoriesData) {
-      setActiveTab(Number(categoryParam))
-    } else {
+    if (!categoryParam && !keywordParam) {
+      setIsLoading(false)
       setActiveTab(0)
+      setKeyword('')
+      setInputData('')
+      return
+    }
+    if (categoryParam) {
+      setActiveTab(Number(categoryParam))
     }
     if (keywordParam) {
       setKeyword(keywordParam)
       setInputData(keywordParam)
-    } else {
-      setKeyword('')
     }
+    setIsLoading(false)
   }, [])
 
   useEffect(() => {
@@ -134,111 +148,119 @@ const ProductList = () => {
 
   return (
     <main>
-      <div className="mb-6 drop-shadow-md">
-        <div className="mt-6 relative flex items-center w-full h-14 rounded-lg shadow hover:shadow-md bg-white overflow-hidden">
-          <div className="grid place-items-center h-full w-14 text-gray-300">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </div>
-          <input
-            className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
-            onKeyDown={onKeyDown}
-            onChange={keywordHandler}
-            value={inputData}
-            spellCheck={false}
-            placeholder="최근에 구매하고 싶은 제품이 있으셨나요?"
-          />
-
-          <div
-            className="grid place-items-center h-full w-14 text-gray-300 cursor-pointer"
-            onClick={handleClose}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </div>
+      {isLoading ? (
+        <div>
+          <ProductLoading />
         </div>
-      </div>
-      <Tabs
-        className="react-tabs__tab-list"
-        forceRenderTabPanel
-        selectedIndex={activeTab}
-        onSelect={handleTabChange}
-        defaultFocus
-        disableUpDownKeys
-      >
-        <TabList
-          className={`will-change-transform scroll-smooth overflow-x-scroll ${
-            isMobile ? 'whitespace-nowrap' : ''
-          }`}
-        >
-          {[allCategory, ...categoriesData.categories].map((category) => (
-            <Tab
-              key={category.id}
-              className="hover:text-zinc-700 transition duration-200 inline-block p-2 text-zinc-400 font-bold b-0"
-              id={`profile-tab-${category.id}`}
-              data-tabs-target={`#profile-${category.id}`}
-              type="button"
-              role="tab"
-              aria-controls={`profile-${category.id}`}
-            >
-              <button>{category.name}</button>
-            </Tab>
-          ))}
-        </TabList>
+      ) : (
+        <>
+          <div className="mb-6 drop-shadow-md">
+            <div className="mt-6 relative flex items-center w-full h-14 rounded-lg shadow hover:shadow-md bg-white overflow-hidden">
+              <div className="grid place-items-center h-full w-14 text-gray-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </div>
+              <input
+                className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+                onKeyDown={onKeyDown}
+                onChange={keywordHandler}
+                value={inputData}
+                spellCheck={false}
+                placeholder="최근에 구매하고 싶은 제품이 있으셨나요?"
+              />
 
-        <div className="flex">
-          <SwipeableViews
-            index={activeTab}
-            onChangeIndex={handleTabChange}
-            animateTransitions={isMobile}
-            className="will-change-transform my-6"
+              <div
+                className="grid place-items-center h-full w-14 text-gray-300 cursor-pointer"
+                onClick={handleClose}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <Tabs
+            className="react-tabs__tab-list"
+            forceRenderTabPanel
+            selectedIndex={activeTab}
+            onSelect={handleTabChange}
+            defaultFocus
+            disableUpDownKeys
           >
-            {[allCategory, ...categoriesData.categories].map((category) => (
-              <TabPanel key={category.id}>
-                {products.length === 0 ? (
-                  <div className="flex">
-                    <ProductNotFound />
-                  </div>
-                ) : (
-                  <div className="flex">
-                    <div className="item-center grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
-                      {products.map((product) => (
-                        <ProductCard key={product.id} product={product} />
-                      ))}
-                    </div>
-                  </div>
-                )}
-              </TabPanel>
-            ))}
-          </SwipeableViews>
-        </div>
-        <TopButton />
-        {hasNextData && <div ref={ref} className="h-[48px] w-full" />}
-      </Tabs>
+            <TabList
+              className={`will-change-transform scroll-smooth overflow-x-scroll ${
+                isMobile ? 'whitespace-nowrap' : ''
+              }`}
+            >
+              {[allCategory, ...categoriesData.categories].map((category) => (
+                <Tab
+                  key={category.id}
+                  className="hover:text-zinc-700 transition duration-200 inline-block p-2 text-zinc-400 font-bold b-0"
+                  id={`profile-tab-${category.id}`}
+                  data-tabs-target={`#profile-${category.id}`}
+                  type="button"
+                  role="tab"
+                  aria-controls={`profile-${category.id}`}
+                >
+                  <button>{category.name}</button>
+                </Tab>
+              ))}
+            </TabList>
+
+            <div className="flex">
+              <SwipeableViews
+                index={activeTab}
+                onChangeIndex={handleTabChange}
+                animateTransitions={isMobile}
+                className="will-change-transform my-6"
+              >
+                {[allCategory, ...categoriesData.categories].map((category) => (
+                  <TabPanel key={category.id}>
+                    {products.length === 0 ? (
+                      <div className="flex min-h-[500px]">
+                        <ProductNotFound />
+                      </div>
+                    ) : (
+                      <div className="fex min-h-[500px]">
+                        <div className=" item-center grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
+                          {products.map((product) => (
+                            <ProductCard key={product.id} product={product} />
+                          ))}
+                        </div>
+                      </div>
+                    )}
+                  </TabPanel>
+                ))}
+              </SwipeableViews>
+            </div>
+            <TopButton />
+            {hasNextData && <div ref={ref} className="h-[48px] w-full" />}
+          </Tabs>
+        </>
+      )}
     </main>
   )
 }
---
+++ src/components/main/productList/ProductLoading.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Lottie from 'lottie-react'
+import loading from '../../../../public/looties/loading.json'
+
+const ProductLoading = () => {
+  return (
+    <div className="w-full pt-[320px] flex justify-center align-center">
+      <div>상품정보를 불러오는 중입니다.</div>
+      {/* <Lottie animationData={loading} className="w-[80px]" /> */}
+    </div>
+  )
+}
+
+export default ProductLoading
