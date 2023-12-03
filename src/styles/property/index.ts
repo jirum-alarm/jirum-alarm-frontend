fa1c9f1d13a89dea2a7df9ba8022ec4da1e7d8ab
yooyeonhee <devyh0629@gmail.com>
2023-12-03 18:36:50 +0900

    Feat: theme 적용

---
+++ src/styles/property/index.ts
@@ -0,0 +1,3 @@
+export * from './clamp';
+export * from './colors';
+export * from './fonts';
---
+++ src/styles/theme.ts
@@ -0,0 +1,7 @@
+import { clamp, colors, fonts } from './property'
+
+export const theme = {
+  clamp,
+  colors,
+  fonts,
+}
