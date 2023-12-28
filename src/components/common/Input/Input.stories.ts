932ac20ca482a8d9ad8ad05d8aaec03c88474826
순민 <tkksm2@gmail.com>
2023-12-28 13:19:07 +0900

    chore: add clsx

--- src/components/common/Input/Input.stories.ts
+++ src/components/common/Input/Input.stories.ts
@@ -12,10 +12,6 @@ const meta = {
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } satisfies Meta<typeof Input>
 
 export default meta
@@ -23,8 +19,5 @@ type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
+  args: {},
 }
--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,3 +1,15 @@
-export const Input = () => {
-  return <div className="text-gray-400">input</div>
+import React from 'react'
+
+interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  variant?: 'standard' | 'outlined'
+  size?: 'md' | 'lg'
+  error?: boolean | string
 }
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>((props: InputProps, ref) => {
+  return (
+    <div>
+      <input ref={ref} className="text-gray-400" />
+    </div>
+  )
+})
