4c710245c3ce1d744d0b462ce940716fa003aac3
tolluset <dlqud19@gmail.com>
2023-12-29 01:58:42 +0900

    chore: reorder lines

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,10 +1,9 @@
 import { cx } from 'class-variance-authority'
 import { twMerge } from 'tailwind-merge'
 
-export type ClassValue = ClassArray | ClassDictionary | string | number | null | boolean | undefined
-export type ClassDictionary = Record<string, any>
-export type ClassArray = ClassValue[]
-
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(cx(inputs))
 }
+export type ClassValue = ClassArray | ClassDictionary | string | number | null | boolean | undefined
+export type ClassDictionary = Record<string, any>
+export type ClassArray = ClassValue[]
