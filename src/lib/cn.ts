220f80e344b905ea63f7bba4eb1e3d679529335c
순민 <tkksm2@gmail.com>
2023-12-29 13:38:33 +0900

    chore: change parameter type of cn function

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,9 +1,6 @@
-import { cx } from 'class-variance-authority'
+import { type CxOptions, cx } from 'class-variance-authority'
 import { twMerge } from 'tailwind-merge'
 
-export const cn = (...inputs: ClassValue[]) => {
+export const cn = (...inputs: CxOptions) => {
   return twMerge(cx(inputs))
 }
-export type ClassValue = ClassArray | ClassDictionary | string | number | null | boolean | undefined
-export type ClassDictionary = Record<string, any>
-export type ClassArray = ClassValue[]
