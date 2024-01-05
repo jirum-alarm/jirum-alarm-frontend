186010996dfce879dabfd3d6a88dc7b8de5dc059
순민 <tkksm2@gmail.com>
2024-01-05 18:24:58 +0900

    feat: add user fetching

--- src/app/mypage/account/page.tsx
+++ src/app/mypage/account/page.tsx
@@ -1,13 +1,17 @@
 import BasicLayout from '@/components/layout/BasicLayout'
 import React from 'react'
 import MovePage from './components/MovePage'
+import { getClient } from '@/lib/client'
+import { User } from '@/types/user'
+import { QueryMe } from '@/graphql/auth'
 
-const AccountPage = () => {
+const AccountPage = async () => {
+  const { data } = await getClient().query<{ me: User }>({ query: QueryMe })
   return (
     <BasicLayout hasBackButton title="가입 정보">
       <div className="h-full px-5 pb-8 flex flex-col">
         <div className="pt-6 pb-8 border-b border-b-gray-300">
-          <MovePage to="/" title="닉네임 " subtitle="사용자닉네임" />
+          <MovePage to="/" title="닉네임 " subtitle={data.me.nickname} />
           <MovePage to="/" title="기본정보 " />
         </div>
         <div className="pt-8 flex-1 flex flex-col">
@@ -15,7 +19,7 @@ const AccountPage = () => {
             <span className="text-sm text-gray-600">이메일 주소</span>
           </div>
           <div>
-            <span className="text-gray-900">tkksm@naver.com</span>
+            <span className="text-gray-900">{data.me.email}</span>
           </div>
           <div className="flex-1 flex flex-col justify-end">
             <div className="flex justify-center">
