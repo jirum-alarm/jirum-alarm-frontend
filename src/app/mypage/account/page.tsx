e1346998e36abe64d5ad8a79b22081a077887ade
순민 <tkksm2@gmail.com>
2024-01-05 15:40:55 +0900

    style: publishing account-info page

---
+++ src/app/mypage/account/page.tsx
@@ -0,0 +1,35 @@
+import BasicLayout from '@/components/layout/BasicLayout'
+import React from 'react'
+import MovePage from './components/MovePage'
+
+const AccountPage = () => {
+  return (
+    <BasicLayout hasBackButton title="가입 정보">
+      <div className="h-full px-5 pb-8 flex flex-col">
+        <div className="pt-6 pb-8 border-b border-b-gray-300">
+          <MovePage to="/" title="닉네임 " subtitle="사용자닉네임" />
+          <MovePage to="/" title="기본정보 " />
+        </div>
+        <div className="pt-8 flex-1 flex flex-col">
+          <div className="pb-[22px]">
+            <span className="text-sm text-gray-600">이메일 주소</span>
+          </div>
+          <div>
+            <span className="text-gray-900">tkksm@naver.com</span>
+          </div>
+          <div className="flex-1 flex flex-col justify-end">
+            <div className="flex justify-center">
+              <div className="flex items-center">
+                <button className="px-6 py-3 text-[13px] text-gray-500">로그아웃</button>
+                <div className="w-px h-3 bg-gray-200" />
+                <button className="px-6 py-3 text-[13px] text-gray-500">회원탈퇴</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BasicLayout>
+  )
+}
+
+export default AccountPage
--- src/components/layout/BasicLayout.tsx
+++ src/components/layout/BasicLayout.tsx
@@ -9,12 +9,12 @@ interface Props {
 
 const BasicLayout = ({ children, title, hasBackButton }: Props) => {
   return (
-    <div className="max-w-[480px] mx-auto bg-white h-full">
+    <div className="max-w-[480px] mx-auto bg-white h-[calc(100dvh)] flex flex-col">
       <header className="h-14 relative flex items-center justify-center bg-white text-black">
         {hasBackButton && <div className="absolute left-0">{<BackButton />}</div>}
         {title && <h1 className="text-base font-semibold text-black">{title}</h1>}
       </header>
-      <div>{children}</div>
+      <div className="flex-1">{children}</div>
     </div>
   )
 }
