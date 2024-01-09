1f1e6d36e3c2fc3a1f7f0361733bb8cae6d5cc18
tolluset <dlqud19@gmail.com>
2024-01-10 03:08:05 +0900

    feat: adjust signup flows

--- src/app/signup/components/Password.tsx
+++ src/app/signup/components/Password.tsx
@@ -3,17 +3,13 @@ import Input from '@/components/common/Input'
 import { Registration } from '../page'
 import { Cancel } from '@/components/common/icons'
 
-type Id = 'email' | 'password'
-
-const RegisterByEmail = ({
+const Password = ({
   registration,
   handleRegistration,
   moveNextStep,
 }: {
   registration: Registration
-  handleRegistration: (
-    passwordAndEmail: (registration: Registration) => Partial<Registration>,
-  ) => void
+  handleRegistration: (password: (registration: Registration) => Partial<Registration>) => void
   moveNextStep: () => void
 }) => {
   const handleInputChange = (
@@ -23,33 +19,28 @@ const RegisterByEmail = ({
     const { id, value } = e.target
     const error = validate(value) ? false : true
 
-    if (id === 'email' || id === 'password') {
+    if (id === 'password') {
       handleRegistration((prev) => ({ [id]: { ...prev[id], value, error } }))
     }
   }
 
-  const handleInputFocus = (id: Id) => {
-    handleRegistration((prev) => ({ [id]: { ...prev[id], focus: true } }))
+  const handleInputFocus = () => {
+    handleRegistration((prev) => ({ password: { ...prev['password'], focus: true } }))
   }
 
-  const handleInputBlur = (id: Id) => {
-    handleRegistration((prev) => ({ [id]: { ...prev[id], focus: false } }))
+  const handleInputBlur = () => {
+    handleRegistration((prev) => ({ password: { ...prev['password'], focus: false } }))
   }
 
-  const reset = (id: 'email' | 'password') => {
-    handleRegistration(() => ({ [id]: { value: '', error: false, focus: false } }))
+  const reset = () => {
+    handleRegistration(() => ({ password: { value: '', error: false, focus: false } }))
   }
 
   const handleCTAButton = () => {
     moveNextStep()
   }
 
-  const isValidInput = !!(
-    registration.email.value &&
-    !registration.email.error &&
-    registration.password.value &&
-    !registration.password.error
-  )
+  const isValidInput = !!(registration.password.value && !registration.password.error)
 
   return (
     <div className="grid h-full">
@@ -57,14 +48,7 @@ const RegisterByEmail = ({
         <Description />
         <div className="grid items-end">
           <form className="grid gap-y-8 pt-[88px]">
-            <Email
-              registration={registration}
-              handleInputChange={handleInputChange}
-              handleInputFocus={handleInputFocus}
-              handleInputBlur={handleInputBlur}
-              reset={reset}
-            />
-            <Password
+            <PasswordInput
               registration={registration}
               handleInputChange={handleInputChange}
               handleInputFocus={handleInputFocus}
@@ -81,19 +65,19 @@ const RegisterByEmail = ({
   )
 }
 
-export default RegisterByEmail
+export default Password
 
 const Description = () => {
   return (
     <p className="font-semibold text-2xl">
-      이메일과 비밀번호를
+      비밀번호를
       <br />
       입력해주세요.
     </p>
   )
 }
 
-const Email = ({
+const PasswordInput = ({
   registration,
   handleInputChange,
   handleInputFocus,
@@ -105,58 +89,10 @@ const Email = ({
     e: React.ChangeEvent<HTMLInputElement>,
     validate: (value: string) => boolean,
   ) => void
-  handleInputFocus: (id: Id) => void
-  handleInputBlur: (id: Id) => void
-  reset: (id: 'email') => void
+  handleInputFocus: () => void
+  handleInputBlur: () => void
+  reset: () => void
 }) => {
-  const id = 'email'
-
-  const validate = (value: string) => {
-    if (value === '') {
-      return true
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-
-    return emailRegex.test(value)
-  }
-
-  return (
-    <label>
-      <Input
-        type="email"
-        id="email"
-        autoComplete="email"
-        placeholder="이메일을 입력해주세요."
-        required
-        value={registration.email.value}
-        icon={registration.email.focus ? <Cancel onMouseDown={() => reset(id)} /> : ''}
-        error={registration.email.error && '올바른 이메일 형식으로 입력해주세요.'}
-        onChange={(e) => handleInputChange(e, validate)}
-        onFocus={() => handleInputFocus(id)}
-        onBlur={() => handleInputBlur(id)}
-      />
-    </label>
-  )
-}
-
-const Password = ({
-  registration,
-  handleInputChange,
-  handleInputFocus,
-  handleInputBlur,
-  reset,
-}: {
-  registration: Registration
-  handleInputChange: (
-    e: React.ChangeEvent<HTMLInputElement>,
-    validate: (value: string) => boolean,
-  ) => void
-  handleInputFocus: (id: Id) => void
-  handleInputBlur: (id: Id) => void
-  reset: (id: 'password') => void
-}) => {
-  const id = 'password'
   const validate = (value: string) => {
     if (value === '') {
       return true
@@ -186,11 +122,11 @@ const Password = ({
         placeholder="비밀번호를 입력해주세요."
         required
         value={registration.password.value}
-        icon={registration.password.focus ? <Cancel onMouseDown={() => reset(id)} /> : ''}
+        icon={registration.password.focus ? <Cancel onMouseDown={reset} /> : ''}
         error={registration.password.error && '영문, 숫자, 특수문자 중 2개 이상 조합해주세요.'}
         onChange={(e) => handleInputChange(e, validate)}
-        onFocus={() => handleInputFocus(id)}
-        onBlur={() => handleInputBlur(id)}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
       />
     </label>
   )
