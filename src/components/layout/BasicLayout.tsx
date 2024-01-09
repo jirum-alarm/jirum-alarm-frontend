ccb0e438d969562cc13e0697923c092769f6f84a
tolluset <dlqud19@gmail.com>
2024-01-10 02:52:29 +0900

    fix: #58 adjust layout height

--- src/components/layout/BasicLayout.tsx
+++ src/components/layout/BasicLayout.tsx
@@ -9,7 +9,10 @@ interface Props {
 
 const BasicLayout = ({ children, title, hasBackButton }: Props) => {
   return (
-    <div className="relative min-h-screen grid max-w-[480px] mx-auto bg-white">
+    /**
+     * @description 레이아웃의 높이는 화면 스크린 - 헤더 높이 (스크롤이 나오지 않게 하기 위해)
+     * */
+    <div className="relative min-h-[calc(100vh-56px)] grid max-w-[480px] mx-auto bg-white">
       <header className="h-14 fixed top-0 z-50 max-w-[480px] w-full flex items-center justify-center bg-white text-black">
         {hasBackButton && <div className="absolute left-0">{<BackButton />}</div>}
         {title && <h1 className="text-base font-semibold text-black">{title}</h1>}
