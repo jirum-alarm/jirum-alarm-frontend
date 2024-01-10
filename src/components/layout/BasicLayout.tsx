39628be285110036a681f1ec85e782e49aa1b827
tolluset <dlqud19@gmail.com>
2024-01-10 17:49:33 +0900

    fix: use util class

--- src/components/layout/BasicLayout.tsx
+++ src/components/layout/BasicLayout.tsx
@@ -16,7 +16,10 @@ const BasicLayout = ({ children, title, hasBackButton, noScroll }: Props) => {
   return (
     <div className={cn('relative grid max-w-[480px] mx-auto bg-white', layoutHeight)}>
       <header
-        className={`h-[${headerHeight}] fixed top-0 z-50 max-w-[480px] w-full flex items-center justify-center bg-white text-black`}
+        // height 변경 시 레이아웃 계산식에 사용하는 변수값도 변경
+        className={
+          'h-14 fixed top-0 z-50 max-w-[480px] w-full flex items-center justify-center bg-white text-black'
+        }
       >
         {hasBackButton && <div className="absolute left-0">{<BackButton />}</div>}
         {title && <h1 className="text-base font-semibold text-black">{title}</h1>}
