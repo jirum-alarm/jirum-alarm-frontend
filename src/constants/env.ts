c5a9e0303e4a0ab30a91adb541401ab1662342da
tolluset <dlqud19@gmail.com>
2024-02-25 19:07:47 +0900

    chore: version up packages resolve #159

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,3 +1,5 @@
 export const IS_API_MOCKING = process.env.NEXT_PUBLIC_API_MOCKING === 'enable';
 export const IS_VERCEL_PRD =
   process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' || process.env.VERCEL_ENV === 'production';
+
+export const SERVICE_URL = process.env.SERVICE_URL ?? 'https://jirum-alarm.com';
