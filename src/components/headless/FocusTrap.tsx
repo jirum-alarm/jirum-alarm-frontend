05c4e35a55713faff83fe67d9431b4cf21b5e9b8
순민 <tkksm2@gmail.com>
2024-02-28 00:14:18 +0900

    fix: focus bug

--- src/components/headless/FocusTrap.tsx
+++ src/components/headless/FocusTrap.tsx
@@ -43,6 +43,7 @@ interface FocusTrapProps extends React.HTMLAttributes<HTMLDivElement> {
 const FocusTrap = React.forwardRef<HTMLDivElement, FocusTrapProps>((props, forwardedRef) => {
   const { children, onEscapeFocusTrap, ...others } = props;
   const focusTrapRef = useRef<HTMLDivElement>(null);
+  const currentFocusIndex = useRef(-1);
   const focusableElements = useRef<(HTMLElement | null)[]>([]);
   const child = React.Children.only(children);
 
@@ -50,20 +51,64 @@ const FocusTrap = React.forwardRef<HTMLDivElement, FocusTrapProps>((props, forwa
   const firstElement = focusableElements.current[0];
   const lastElement = focusableElements.current.at(-1);
 
+  const focusNextElement = () => {
+    const element = focusableElements.current[currentFocusIndex.current + 1];
+    if (!element) {
+      currentFocusIndex.current = 0;
+      firstElement?.focus();
+      return;
+    }
+    element.focus();
+    currentFocusIndex.current++;
+  };
+
+  const focusPrevElement = () => {
+    const element = focusableElements.current[currentFocusIndex.current - 1];
+    if (!element) {
+      currentFocusIndex.current = focusableElements.current.length - 1;
+      lastElement?.focus();
+      return;
+    }
+    element.focus();
+    currentFocusIndex.current--;
+  };
+
+  /**
+   * tab key down event
+   */
+  const handleTabKeyDown = (event: KeyboardEvent) => {
+    const isTabKeyDown = !event.shiftKey && event.key === 'Tab';
+    if (!isTabKeyDown) return;
+
+    event.preventDefault();
+    focusNextElement();
+  };
+
+  /**
+   * shift+tab key down event
+   */
+  const handleShiftTabKeyDown = (event: KeyboardEvent) => {
+    const isShiftTabKeyDown = event.shiftKey && event.key === 'Tab';
+    if (!isShiftTabKeyDown) return;
+
+    event.preventDefault();
+    focusPrevElement();
+  };
+
+  /**
+   * escape key down event
+   */
+  const handleEscapeKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      onEscapeFocusTrap();
+    }
+  };
+
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
-      if (event.key === 'Tab') {
-        if (event.shiftKey && document.activeElement === firstElement) {
-          event.preventDefault();
-          lastElement?.focus();
-        } else if (!event.shiftKey && document.activeElement === lastElement) {
-          event.preventDefault();
-          firstElement?.focus();
-        }
-      }
-      if (event.key === 'Escape') {
-        onEscapeFocusTrap();
-      }
+      handleTabKeyDown(event);
+      handleShiftTabKeyDown(event);
+      handleEscapeKeyDown(event);
     };
     document.addEventListener('keydown', handleKeyPress);
     return () => document.removeEventListener('keydown', handleKeyPress);
