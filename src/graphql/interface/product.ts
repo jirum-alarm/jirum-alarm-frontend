f80bb024134d17c71e4278ac5e7f7dc2be998008
tolluset <dlqud19@gmail.com>
2024-05-04 11:17:52 +0900

    feat: add home design v2

--- src/graphql/interface/product.ts
+++ src/graphql/interface/product.ts
@@ -6,6 +6,10 @@ export interface IProduct {
   provider: IProvider;
   mallId?: number;
   url: string;
+  isHot: boolean;
+  isEnd: boolean;
+  ship: string;
+  price: string;
   providerId: number;
   thumbnail?: string;
   category?: string;
---
+++ src/hooks/useScreenSize.ts
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+
+type ScreenSizes = {
+  sm: boolean;
+  md: boolean;
+  lg: boolean;
+  xl: boolean;
+  // 2xl
+  txl: boolean;
+};
+
+const breakpoints: { [key in keyof ScreenSizes]: string } = {
+  sm: '(min-width: 640px)',
+  md: '(min-width: 768px)',
+  lg: '(min-width: 1024px)',
+  xl: '(min-width: 1280px)',
+  txl: '(min-width: 1536px)',
+};
+
+/**
+ * @description tailiwndcss breakpoints를 따르는 화면 사이즈 확인 훅
+ * @example const { sm, md, lg, xl, txl } = useScreen();
+ * */
+function useScreen(): ScreenSizes {
+  const [screenSizes, setScreenSizes] = useState<ScreenSizes>({
+    sm: false,
+    md: false,
+    lg: false,
+    xl: false,
+    txl: false,
+  });
+
+  useEffect(() => {
+    const mediaQueries: { [key in keyof ScreenSizes]: MediaQueryList } = {
+      sm: window.matchMedia(breakpoints.sm),
+      md: window.matchMedia(breakpoints.md),
+      lg: window.matchMedia(breakpoints.lg),
+      xl: window.matchMedia(breakpoints.xl),
+      txl: window.matchMedia(breakpoints.txl),
+    };
+
+    function updateMatch(): void {
+      setScreenSizes({
+        sm: mediaQueries.sm.matches,
+        md: mediaQueries.md.matches,
+        lg: mediaQueries.lg.matches,
+        xl: mediaQueries.xl.matches,
+        txl: mediaQueries.txl.matches,
+      });
+    }
+
+    updateMatch();
+
+    Object.values(mediaQueries).forEach((mq) => mq.addEventListener('change', updateMatch));
+
+    return () => {
+      Object.values(mediaQueries).forEach((mq) => mq.removeEventListener('change', updateMatch));
+    };
+  }, []);
+
+  return screenSizes;
+}
+
+export default useScreen;
