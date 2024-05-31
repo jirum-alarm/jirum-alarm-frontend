37fab34c379121599deb935ea499483268911742
tolluset <dlqud19@gmail.com>
2024-05-31 12:13:49 +0900

    fix: redirect home when logout

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -5,7 +5,7 @@ export const useLogout = () => {
   const handleLogout = () => {
     localStorage.removeItem(StorageTokenKey.ACCESS_TOKEN);
     localStorage.removeItem(StorageTokenKey.REFRESH_TOKEN);
-    window.location.replace(PAGE.LOGIN);
+    window.location.replace(PAGE.HOME);
   };
   return handleLogout;
 };
