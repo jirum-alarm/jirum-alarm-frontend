87d811c943bfdc82c607f1a49fe669a3c3eb2cce
tolluset <dlqud19@gmail.com>
2024-06-02 23:45:44 +0900

    chore: test disable tunnel

--- next.config.js
+++ next.config.js
@@ -37,7 +37,7 @@ const sentryBuildTimeConfigOptions = {
   // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: '/monitoring',
+  // tunnelRoute: '/monitoring',
 
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
