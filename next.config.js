b9a715c6cd9a214e1843b6920a5e9a4f10429ee7
tolluset <dlqud19@gmail.com>
2024-06-03 00:39:36 +0900

    chore: rollback sentry

--- next.config.js
+++ next.config.js
@@ -37,7 +37,7 @@ const sentryBuildTimeConfigOptions = {
   // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  // tunnelRoute: '/monitoring',
+  tunnelRoute: '/monitoring',
 
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
@@ -91,5 +91,4 @@ const nextConfig = withPWA({
   },
 });
 
-// module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
-module.exports = nextConfig;
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
