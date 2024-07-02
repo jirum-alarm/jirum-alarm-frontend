202bfba7cd2909a6fb84fa06f34a2697314206f9
SoonMin <89559826+Ssoon-m@users.noreply.github.com>
2024-07-02 21:24:49 +0900

    style(admin): 어드민 레이아웃 버그 수정 (#214)
    * style: delete 100vh style
    
    * fix: admin layout

--- apps/admin/src/components/Sidebar/index.tsx
+++ apps/admin/src/components/Sidebar/index.tsx
@@ -57,7 +57,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return (
     <aside
       ref={sidebar}
-      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:fixed lg:translate-x-0 ${
+      className={`fixed left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:translate-x-0 ${
         sidebarOpen ? 'translate-x-0' : '-translate-x-full'
       }`}
     >
