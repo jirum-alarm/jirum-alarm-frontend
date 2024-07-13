45c6243ff1f4a9aaf71f0f711e26a04d5d55633f
순민 <tkksm2@gmail.com>
2024-07-13 19:16:52 +0900

    feat: feature flag conditional

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import AddFCMToken from './components/AddFCMToken';
 import HomeContainer from './components/HomeContainer';
 import HomeContainerV2 from './components/(home)/HomeContainerV2';
 import { getFeatureFlag } from '../actions/posthog';
+import { IS_VERCEL_PRD } from '@/constants/env';
 
 export default async function Home() {
   const { flags } = await getFeatureFlag();
@@ -9,7 +10,7 @@ export default async function Home() {
   return (
     <>
       <AddFCMToken />
-      {flags.MAIN_PAGE_RENEWAL_FEATURE ? <HomeContainerV2 /> : <HomeContainer />}
+      {!IS_VERCEL_PRD && flags.MAIN_PAGE_RENEWAL_FEATURE ? <HomeContainerV2 /> : <HomeContainer />}
       {/* <HomeContainer /> */}
     </>
   );
