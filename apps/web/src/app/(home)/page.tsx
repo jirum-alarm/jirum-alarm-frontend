bec7fe1986d2b842738ca5da81d0d69524f73c25
순민 <tkksm2@gmail.com>
2024-07-14 15:19:12 +0900

    feat: filtering and normalization logic

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -2,7 +2,6 @@ import AddFCMToken from './components/AddFCMToken';
 import HomeContainer from './components/HomeContainer';
 import HomeContainerV2 from './components/(home)/HomeContainerV2';
 import { getFeatureFlag } from '../actions/posthog';
-import { IS_VERCEL_PRD } from '@/constants/env';
 
 export default async function Home() {
   const { flags } = await getFeatureFlag();
@@ -10,7 +9,7 @@ export default async function Home() {
   return (
     <>
       <AddFCMToken />
-      {!IS_VERCEL_PRD && flags.MAIN_PAGE_RENEWAL_FEATURE ? <HomeContainerV2 /> : <HomeContainer />}
+      {flags.MAIN_PAGE_RENEWAL_FEATURE ? <HomeContainerV2 /> : <HomeContainer />}
       {/* <HomeContainer /> */}
     </>
   );
