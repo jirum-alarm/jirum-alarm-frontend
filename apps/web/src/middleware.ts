ef1f3a3bd352dee9def875298b02a2d611dcbf9c
Ssoon-m <tkksm2@gmail.com>
2024-07-19 10:17:40 +0900

    fix: handle cookie setting in middleware for server action access

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,5 +1,27 @@
 // middleware.js
 import { NextRequest, NextResponse } from 'next/server';
+import { ResponseCookies, RequestCookies } from 'next/dist/server/web/spec-extension/cookies';
+
+/**
+ * Copy cookies from the Set-Cookie header of the response to the Cookie header of the request,
+ * so that it will appear to SSR/RSC as if the user already has the new cookies.
+ */
+function applySetCookie(req: NextRequest, res: NextResponse): void {
+  // parse the outgoing Set-Cookie header
+  const setCookies = new ResponseCookies(res.headers);
+  // Build a new Cookie header for the request by adding the setCookies
+  const newReqHeaders = new Headers(req.headers);
+  const newReqCookies = new RequestCookies(newReqHeaders);
+  setCookies.getAll().forEach((cookie) => newReqCookies.set(cookie));
+  // set “request header overrides” on the outgoing response
+  NextResponse.next({
+    request: { headers: newReqHeaders },
+  }).headers.forEach((value, key) => {
+    if (key === 'x-middleware-override-headers' || key.startsWith('x-middleware-request-')) {
+      res.headers.set(key, value);
+    }
+  });
+}
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
   const ph_project_api_key = process.env.NEXT_PUBLIC_POSTHOG_KEY;
@@ -8,7 +30,7 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
 
   let distinct_id;
   if (cookie) {
-    distinct_id = JSON.parse(cookie.value).distinct_id;
+    distinct_id = JSON.parse(cookie?.value).distinct_id;
   } else {
     distinct_id = crypto.randomUUID();
   }
@@ -31,13 +53,24 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
 
   const data = await ph_request.json();
 
+  const response = NextResponse.next({
+    request: {
+      headers: new Headers(request.headers),
+    },
+  });
+
   const bootstrapData = {
     distinctID: distinct_id,
     featureFlags: data.featureFlags,
   };
 
-  const response = NextResponse.next();
-  response.cookies.set('bootstrapData', JSON.stringify(bootstrapData));
+  const cookieInfo = {
+    name: 'bootstrapData',
+    value: JSON.stringify(bootstrapData),
+  };
+
+  response.cookies.set(cookieInfo);
+  applySetCookie(request, response);
 
   return response;
 }
