207da1668292dee5b5257130d98a2429c531a50b
Ssoon-m <tkksm2@gmail.com>
2024-07-19 09:27:43 +0900

    test

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import AddFCMToken from './components/AddFCMToken';
 import HomeContainer from './components/HomeContainer';
 import HomeContainerV2 from './components/(home)/HomeContainerV2';
 import { getFeatureFlag } from '../actions/posthog';
+// .
 
 export default async function Home() {
   const { flags } = await getFeatureFlag();
