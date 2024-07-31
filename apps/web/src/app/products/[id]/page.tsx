31b36d524447ed73c3fc6a65e9d492ed3ab1f60b
Lee Byonghun <dlqud19@gmail.com>
2024-08-01 05:04:13 +0900

    feature(web): 상품 상세 페이지 메타데이터 추가 (#226)
    * feat: add metadata
    
    * chore: add default description
    
    * chore: adjust og image
    
    * chore: add url and type

--- apps/web/src/app/products/[id]/page.tsx
+++ apps/web/src/app/products/[id]/page.tsx
@@ -1,6 +1,43 @@
 import BasicLayout from '@/components/layout/BasicLayout';
 import ProductDetailContainer from './components/ProductDetailContainer';
 import { getFeatureFlag } from '@/app/actions/posthog';
+import { Metadata } from 'next';
+import { getProductDetail } from '@/features/products/server/productDetail';
+import { SERVICE_URL } from '@/constants/env';
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const { flags } = await getFeatureFlag();
+
+  if (flags.DETAIL_PAGE_RENEWAL) {
+    return {
+      title: '지름알림',
+    };
+  }
+
+  const id = params.id;
+
+  const { data } = await getProductDetail(+id);
+  const product = data.product;
+
+  const title = `${product.title} | 지름알림`;
+  const description =
+    product.guides?.map((guide) => guide.content).join(', ') || '핫딜 정보를 알려드려요!';
+  const image = product.thumbnail || '/opengraph-image.png';
+
+  return {
+    title,
+    openGraph: {
+      title,
+      description,
+      url: `${SERVICE_URL}/products/${id}`,
+      type: 'website',
+      images: [{ url: image }],
+    },
+    icons: {
+      icon: '/icon.png',
+    },
+  };
+}
 
 export default async function ProductDetail({ params }: { params: { id: string } }) {
   const { flags } = await getFeatureFlag();
