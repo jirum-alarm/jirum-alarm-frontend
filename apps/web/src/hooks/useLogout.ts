2c5ce7b780749a718b06deccd1e1c7734517ee3f
순민 <tkksm2@gmail.com>
2024-08-07 23:11:51 +0900

    fix: token remove

--- apps/web/src/hooks/useLogout.ts
+++ apps/web/src/hooks/useLogout.ts
@@ -2,9 +2,9 @@ import { removeAccessToken, removeRefreshToken } from '@/app/actions/token';
 import { PAGE } from '@/constants/page';
 
 export const useLogout = () => {
-  const logout = () => {
-    removeAccessToken();
-    removeRefreshToken();
+  const logout = async () => {
+    await removeRefreshToken();
+    await removeAccessToken();
     window.location.replace(PAGE.HOME);
   };
   return logout;
