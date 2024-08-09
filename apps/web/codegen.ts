63c1c468ec2c0f9da0511ba0a9a6a4cfea36ec73
Ssoon-m <tkksm2@gmail.com>
2024-08-09 15:27:12 +0900

    chore: add graphql codegen

---
+++ apps/web/codegen.ts
@@ -0,0 +1,24 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'https://jirum-dev-api.kyojs.com/graphql',
+  documents: ['./src/graphql/*.ts'],
+  hooks: { afterOneFileWrite: ['prettier --write'] },
+  ignoreNoDocuments: true,
+  generates: {
+    './src/shared/api/gql/': {
+      preset: 'client',
+      config: {
+        documentMode: 'string',
+      },
+    },
+    './schema.graphql': {
+      plugins: ['schema-ast'],
+      config: {
+        includeDirectives: true,
+      },
+    },
+  },
+};
+export default config;
