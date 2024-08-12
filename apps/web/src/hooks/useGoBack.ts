72b683a8f33a872becfbc6377119b130df33c39a
tolluset <dlqud19@gmail.com>
2024-08-12 23:10:43 +0900

    fix: back to page

--- apps/web/src/hooks/useGoBack.ts
+++ apps/web/src/hooks/useGoBack.ts
@@ -5,7 +5,8 @@ import { useCallback } from 'react';
 const useGoBack = (backTo: PAGE = PAGE.HOME) => {
   const router = useRouter();
   const goBack = useCallback(() => {
-    document.referrer && document.referrer.indexOf('jirum-alarm.com') != -1
+    (document.referrer && document.referrer.indexOf('jirum-alarm.com') != -1) ||
+    window.history.length > 1
       ? router.back()
       : router.push(backTo);
   }, [router, backTo]);
