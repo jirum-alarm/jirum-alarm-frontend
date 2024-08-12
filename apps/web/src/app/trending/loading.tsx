da521d0ed915fea539c9c952f3bd234c88709a22
tolluset <dlqud19@gmail.com>
2024-08-12 21:25:19 +0900

    chore: add laoding page header

---
+++ apps/web/src/app/trending/loading.tsx
@@ -0,0 +1,10 @@
+import BasicLayout from '@/components/layout/BasicLayout';
+import TrendingPageHeader from './components/TrendingPageHeader';
+
+export default function Loading() {
+  return (
+    <BasicLayout header={<TrendingPageHeader />}>
+      <></>
+    </BasicLayout>
+  );
+}
