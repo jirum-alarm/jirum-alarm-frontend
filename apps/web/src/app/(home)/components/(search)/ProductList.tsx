99b467074206a66415a0218f0925f83105d99d90
tolluset <dlqud19@gmail.com>
2024-08-12 23:19:57 +0900

    fix: serach items layout

--- apps/web/src/app/(home)/components/(search)/ProductList.tsx
+++ apps/web/src/app/(home)/components/(search)/ProductList.tsx
@@ -6,7 +6,7 @@ export default function ProductList({ products }: { products: IProduct[] }) {
   const collectProduct = useCollectProduct();
 
   return (
-    <div className="grid grid-cols-2 justify-items-center gap-x-3 gap-y-5 sm:grid-cols-3 md:grid-cols-4 md:gap-x-5 lg:grid-cols-5 lg:gap-x-6">
+    <div className="grid grid-cols-2 justify-items-center gap-x-3 gap-y-5 sm:grid-cols-3 md:grid-cols-3 md:gap-x-5">
       {products.map((product, i) => (
         <ProductImageCard
           key={i}
