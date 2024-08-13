029b6add315b2c23f31dfd0b2f45f684ea3e581a
순민 <tkksm2@gmail.com>
2024-08-13 21:32:11 +0900

    feat: add adjustStartDate for ranking products

--- apps/web/src/app/trending/hooks/useTrendingViewModel.ts
+++ apps/web/src/app/trending/hooks/useTrendingViewModel.ts
@@ -8,6 +8,14 @@ const TRENDING_ITEMS_LIMIT = 50;
 const HOT_DEAL_COUNT_RANDOM = 20;
 const HOT_DEAL_LIMIT_RANDOM = 10;
 
+//NOTE: 3 : 화장품 , 5 : 도서 , 7 : 등산레저 , 8 : 상품권 , 10 : 육아
+const ExtendStartDateCategories = [3, 5, 7, 8, 10];
+const adjustStartDate = (categoryId: number | null) => {
+  return categoryId !== null && ExtendStartDateCategories.includes(categoryId)
+    ? getDayBefore(60)
+    : getDayBefore(2);
+};
+
 const useTrendingViewModel = ({ categoryId }: { categoryId: number | null }) => {
   const isHotCategory = categoryId === null;
   const { smd } = useScreen();
@@ -18,13 +26,13 @@ const useTrendingViewModel = ({ categoryId }: { categoryId: number | null }) => 
       ProductQueries.products({
         limit: TRENDING_ITEMS_LIMIT,
         orderBy: ProductOrderType.CommunityRanking,
-        startDate: getDayBefore(2),
+        startDate: adjustStartDate(categoryId),
         categoryId: categoryId,
         orderOption: OrderOptionType.Desc,
         isEnd: false,
       }),
       ProductQueries.products({
-        limit: 18,
+        limit: 10,
         orderBy: ProductOrderType.PostedAt,
         categoryId: isHotCategory ? null : categoryId,
       }),
