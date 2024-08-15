e1d01b3e9bd5270c9e4481d2245f54ef0c9c4a86
순민 <tkksm2@gmail.com>
2024-08-15 18:54:00 +0900

    feat: BottomNav animation

--- apps/web/src/components/layout/BottomNav.tsx
+++ apps/web/src/components/layout/BottomNav.tsx
@@ -14,7 +14,8 @@ import { PAGE } from '@/constants/page';
 import { cn } from '@/lib/cn';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import React from 'react';
+import { motion } from 'framer-motion';
+import React, { startTransition, useEffect, useRef, useState } from 'react';
 
 export enum NAV_TYPE {
   HOME = 'HOME',
@@ -54,45 +55,74 @@ const BottomNavList = [
   },
 ] as const;
 
+// 1. 링크를 기준으로 active
+// 2. touch start나 mouse down으로 active 후 링크가 이동 안 됐으면 unactive
+
 const BottomNav = ({ type }: { type: any }) => {
+  const [navType, setNavType] = useState<NAV_TYPE | undefined>(undefined);
+  const previousNavType = useRef<NAV_TYPE | undefined>(navType);
+
   const pathName = usePathname();
+  const navRef = useRef<HTMLUListElement>(null);
+
+  if (!BottomNavList.some((nav) => nav.link === pathName)) return;
 
   {
     /* @TODO: remove afeter v1.1.0 QA */
   }
   // if (IS_VERCEL_PRD) return;
 
-  if (!BottomNavList.some((nav) => nav.link === pathName)) return;
-
-  const isActivePath = (link: PAGE) => {
-    return link === pathName;
+  const handleActiveNav = (type: NAV_TYPE) => () => {
+    console.log('handleActiveNav');
+    previousNavType.current = navType;
+    setNavType(type);
   };
+
+  const handleActiveNavCancel = () => {
+    console.log('handleActiveNavCancel');
+    setNavType(previousNavType.current);
+  };
+
+  const isActiveNav = (type: NAV_TYPE, link: string) => {
+    return navType ? type === navType : link === pathName;
+  };
+
   return (
     <div
       className={cn(
         `fixed bottom-0 left-1/2 z-50 mx-auto w-full max-w-screen-layout-max -translate-x-1/2 border-t border-t-[#D0D5DD] bg-white pb-safe-bottom transition-transform`,
       )}
     >
-      <ul className="flex items-center justify-around">
+      <ul className="flex items-center justify-around" ref={navRef}>
         {BottomNavList.map((nav, i) => (
-          <li className="flex flex-1 items-center justify-center py-3" key={i}>
-            <Link
-              className={cn(
-                // px-5
-                'flex h-[46px] w-[68px] flex-col items-center justify-center rounded-lg text-gray-500',
-                {
-                  'text-gray-900': isActivePath(nav.link),
-                },
-              )}
-              href={nav.link}
+          <li key={i} className="flex flex-1 items-center justify-center py-3">
+            <motion.div
+              className="rounded-lg"
+              whileTap={{ scale: 0.9, backgroundColor: '#F2F4F7' }}
+              transition={{ backgroundColor: { duration: 0 } }} // 애니메이션 없이 즉각적인 배경색 변경
+              onPointerDown={handleActiveNav(nav.type)}
+              onPointerCancel={handleActiveNavCancel}
             >
-              <button className="h-7 w-7">
-                {React.createElement(isActivePath(nav.link) ? nav.activeIcon : nav.icon)}
-              </button>
-              <span className={cn('text-xs', { 'font-semibold': isActivePath(nav.link) })}>
-                {nav.text}
-              </span>
-            </Link>
+              <Link
+                data-nav-type={nav.type}
+                className={cn(
+                  'flex h-[46px] w-[68px] flex-col items-center justify-center rounded-lg text-gray-500',
+                  {
+                    'text-gray-900': isActiveNav(nav.type, nav.link),
+                  },
+                )}
+                href={nav.link}
+              >
+                <button className="h-7 w-7">
+                  {React.createElement(isActiveNav(nav.type, nav.link) ? nav.activeIcon : nav.icon)}
+                </button>
+                <span
+                  className={cn('text-xs', { 'font-semibold': isActiveNav(nav.type, nav.link) })}
+                >
+                  {nav.text}
+                </span>
+              </Link>
+            </motion.div>
           </li>
         ))}
       </ul>
