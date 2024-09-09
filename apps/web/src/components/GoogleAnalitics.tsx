d26e33522dea3ac9ca1d9ae7a5a074c9f632c48a
tjsry0466 <tjs0466@naver.com>
2024-09-10 02:08:19 +0900

    misc: modify ga strategy and add parameter options

--- apps/web/src/components/GoogleAnalitics.tsx
+++ apps/web/src/components/GoogleAnalitics.tsx
@@ -4,13 +4,23 @@ import Script from 'next/script';
 const GoogleAnalytics = ({ GA_TRACKING_ID }: { GA_TRACKING_ID: string }) => {
   return (
     <>
-      {/* Google Analytics 스크립트 */}
-      <Script src={`https://www.google-analytics.com/analytics.js`} strategy="lazyOnload" />
-      <Script id="google-analytics" strategy="lazyOnload">
+      {/* Google Analytics gtag.js */}
+      <Script
+        src={`https://www.google-analytics.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
-        window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};
-          ga('create', '${GA_TRACKING_ID}', 'auto');
-          ga('send', 'pageview');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          // UTM 파라미터 포함한 페이지 경로 설정
+          const pagePath = window.location.pathname + window.location.search;
+
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: pagePath,
+          });
         `}
       </Script>
     </>
