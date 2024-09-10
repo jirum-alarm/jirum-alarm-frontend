d281e50090f3fb7a57fe269b8c6a9478b45a8a14
tjsry0466 <tjs0466@naver.com>
2024-09-10 22:15:04 +0900

    misc: remove unnecessary import

--- apps/web/src/app/trending/components/TrendingList.tsx
+++ apps/web/src/app/trending/components/TrendingList.tsx
@@ -3,7 +3,6 @@ import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 
 import useTrendingViewModel from '../hooks/useTrendingViewModel';
 
-// import { LoadingSpinner } from '@/components/common/icons';
 import RecommendationProduct from '@/app/(home)/components/(search)/RecommendationProduct';
 import { ProductImageCard, ProductTrendingImageCard, useCollectProduct } from '@/features/products';
 
