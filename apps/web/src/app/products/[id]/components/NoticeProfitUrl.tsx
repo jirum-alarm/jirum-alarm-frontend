4bdcde59fbeac23efee661eea6f27e0d83609d8e
tjsry0466 <tjs0466@naver.com>
2024-09-12 13:34:21 +0900

    style: modify notice commisition font style

--- apps/web/src/app/products/[id]/components/NoticeProfitUrl.tsx
+++ apps/web/src/app/products/[id]/components/NoticeProfitUrl.tsx
@@ -1,11 +1,11 @@
 export function NoticeProfitLink() {
   return (
     <section>
-      <div className="bg-gray-50 px-5 py-4 pb-4">
-        <p className="mb-2 text-sm font-medium text-gray-900">안내</p>
+      <div className="bg-gray-50 px-5 py-2 pb-4">
+        <p className="text-black-500 mb-2 text-sm font-semibold">안내</p>
         <div className="flex">
           <span className="mr-2 text-sm text-gray-600">•</span>
-          <p className="text-sm text-gray-600">
+          <p className="text-sm font-medium text-gray-500">
             일부 링크는 제휴 마케팅이 적용되어 지름알림에 커미션이 지급될 수 있습니다.
           </p>
         </div>
