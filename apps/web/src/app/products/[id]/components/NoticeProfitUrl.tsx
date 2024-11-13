5919f101e6b40bb3eaaa825d6e399115d11a41eb
Ssoon-m <tkksm2@gmail.com>
2024-11-13 16:45:23 +0900

    style: notice area

--- apps/web/src/app/products/[id]/components/NoticeProfitUrl.tsx
+++ apps/web/src/app/products/[id]/components/NoticeProfitUrl.tsx
@@ -1,11 +1,14 @@
 export function NoticeProfitLink() {
   return (
     <section>
-      <div className="bg-gray-50 px-5 py-4 pb-4">
-        <p className="text-black-500 mb-2 text-sm font-semibold">안내</p>
+      <div className="bg-gray-50 p-5 pb-8 text-gray-600">
+        <p className="mb-1 text-sm font-semibold">안내</p>
+        <ul>
+          <li className=""></li>
+        </ul>
         <div className="flex">
-          <span className="mr-2 text-sm text-gray-600">•</span>
-          <p className="text-sm font-medium text-gray-500">
+          <span className="mr-2 text-sm">•</span>
+          <p className="text-sm">
             일부 링크는 제휴 마케팅이 적용되어 지름알림에 커미션이 지급될 수 있습니다.
           </p>
         </div>
