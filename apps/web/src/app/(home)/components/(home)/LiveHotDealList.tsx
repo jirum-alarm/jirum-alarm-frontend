24bf3ae8dc12c2947bd665be94cffafeac8eea0f
Ssoon-m <tkksm2@gmail.com>
2024-11-22 10:24:17 +0900

    fix: main page layout

--- apps/web/src/app/(home)/components/(home)/LiveHotDealList.tsx
+++ apps/web/src/app/(home)/components/(home)/LiveHotDealList.tsx
@@ -4,16 +4,19 @@ import useLiveHotDealsViewModel from '../../hooks/useLiveHotDealsViewModel';
 import { LoadingSpinner } from '@/components/common/icons';
 import { ProductLiveHotdealsImageCard, useCollectProduct } from '@/features/products';
 import AppDownloadCTA from './AppDownloadCTA';
+import useScreen from '@/hooks/useScreenSize';
 
 const LiveHotDealList = () => {
   const { products, loadingCallbackRef, isFetchingNextPage } = useLiveHotDealsViewModel();
+  const { smd } = useScreen();
+  const firstRenderingCount = smd ? 6 : 4;
   const collectProduct = useCollectProduct();
 
   return (
     <>
       <div className="grid grid-cols-2 justify-items-center gap-x-3 gap-y-5 smd:grid-cols-3">
         {products
-          ?.slice(0, 4)
+          ?.slice(0, firstRenderingCount)
           .map((product, i) => (
             <ProductLiveHotdealsImageCard
               key={i}
@@ -25,7 +28,7 @@ const LiveHotDealList = () => {
       </div>
       <AppDownloadCTA />
       <div className="mt-[20px] grid grid-cols-2 justify-items-center gap-x-3 gap-y-5 smd:grid-cols-3">
-        {products.slice(4).map((product, i) => (
+        {products.slice(firstRenderingCount).map((product, i) => (
           <ProductLiveHotdealsImageCard
             key={i}
             product={product}
