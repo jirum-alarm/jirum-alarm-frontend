6c9c5713a188b4533daef595bef88dec0af57c2f
Ssoon-m <tkksm2@gmail.com>
2025-01-05 01:38:13 +0900

    fix: keyword params null

--- apps/web/src/app/(home)/components/(home)/RecommendedProduct/RecommendedMoreLink.tsx
+++ apps/web/src/app/(home)/components/(home)/RecommendedProduct/RecommendedMoreLink.tsx
@@ -6,8 +6,9 @@ import useTabQueryString from '@/hooks/useTabQueryString';
 
 const RecommendedMoreLink = ({ children }: PropsWithChildren) => {
   const { currentTab } = useTabQueryString('keyword');
+  const queryString = currentTab ? `?keyword=${currentTab}` : '';
   return (
-    <Link className="text-sm text-gray-500" href={`${PAGE.RECOMMEND}?keyword=${currentTab}`}>
+    <Link className="text-sm text-gray-500" href={`${PAGE.RECOMMEND}${queryString}`}>
       {children}
     </Link>
   );
