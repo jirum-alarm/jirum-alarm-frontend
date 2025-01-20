8c03e222cb4c56a07cc612a906b6dcdfdfcff95e
Ssoon-m <tkksm2@gmail.com>
2025-01-20 23:10:33 +0900

    chore: add rnx-kit to support pnpm

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,4 +1,5 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {makeMetroConfig} = require('@rnx-kit/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
 
 /**
  * Metro configuration
@@ -6,6 +7,9 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = makeMetroConfig({
+  // projectRoot: __dirname,
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+  },
+});
