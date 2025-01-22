dd04472324210a70c8b25a81166ae3352076d3da
Ssoon-m <tkksm2@gmail.com>
2025-01-22 22:38:25 +0900

    feat: add userAgent & open external links in the default browser

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-
 import WebView from 'react-native-webview';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {Linking, Platform, SafeAreaView, StyleSheet} from 'react-native';
+import type {ShouldStartLoadRequest} from 'react-native-webview/lib/WebViewTypes';
+
+const userAgent =
+  Platform.OS === 'ios'
+    ? 'IOS Flutter Webview Jirum Alarm'
+    : 'Android Flutter Webview Jirum Alarm';
 
 function App(): React.JSX.Element {
+  const handleShouldStartLoadWithRequest = (event: ShouldStartLoadRequest) => {
+    if (
+      !event.url.includes('jirum-alarm') ||
+      !event.url.startsWith('https://jirum-alarm.com')
+    ) {
+      Linking.openURL(event.url); // 외부 브라우저에서 열기
+      return false;
+    }
+    return true;
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <WebView source={{uri: 'https://jirum-alarm.com/'}} />
+      <WebView
+        source={{uri: 'https://jirum-alarm.com/'}}
+        userAgent={userAgent}
+        onShouldStartLoadWithRequest={handleShouldStartLoadWithRequest}
+      />
     </SafeAreaView>
   );
 }
