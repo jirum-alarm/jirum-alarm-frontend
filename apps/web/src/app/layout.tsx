d5f2d6dffc9e3edc9a1cdf574a77587cbd79efae
tjsry0466 <tjs0466@naver.com>
2025-03-24 23:42:32 +0900

    perf: optimize loading js

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -7,22 +7,23 @@ import { GA_TRACKING_ID } from '@/constants/ga';
 import { defaultMetadata } from '@/constants/metadata';
 import { pretendard } from '@/lib/fonts';
 import BottomNav from '@/components/layout/BottomNav';
-import { headers } from 'next/headers';
 import { checkJirumAlarmApp } from '@/app/actions/agent';
-
-const SpeedInsights = dynamic(
-  () => import('@vercel/speed-insights/next').then((mod) => mod.SpeedInsights),
-  { ssr: false },
-);
 
 const GoogleAnalytics = dynamic(() => import('../components/GoogleAnalitics'), { ssr: false });
 
-const Toaster = dynamic(() => import('@/components/common/Toast/Toaster'), { ssr: false });
+const Toaster = dynamic(() => import('@/components/common/Toast/Toaster'), {
+  ssr: false,
+  loading: () => null,
+});
 
-const MSWInit = dynamic(() => import('@/components/MSWInit'), { ssr: false });
+const MSWInit = dynamic(() => import('@/components/MSWInit'), {
+  ssr: false,
+  loading: () => null,
+});
 
 const InitMixpanel = dynamic(() => import('@/lib/mixpanel').then((mod) => mod.InitMixpanel), {
   ssr: false,
+  loading: () => null,
 });
 
 // const PostHogPageView = dynamic(() => import('@/components/PostHogPageView'), {
@@ -48,17 +49,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <MSWInit>
           <AppProvider>
-            <InitMixpanel />
             <div className="relative min-w-[320px] bg-white before:fixed before:inset-y-0 before:left-1/2 before:z-50 before:w-[1px] before:translate-x-[300px] before:bg-gray-100 after:fixed after:inset-y-0 after:right-1/2 after:z-50 after:w-[1px] after:-translate-x-[300px] after:bg-gray-100">
               {children}
               {!isJirumAlarmApp && <BottomNav type={''} />}
             </div>
             <Toaster />
-            <SpeedInsights />
             {/* <PostHogPageView /> */}
           </AppProvider>
         </MSWInit>
         <GoogleAnalytics GA_TRACKING_ID={GA_TRACKING_ID} />
+        <InitMixpanel />
       </body>
     </html>
   );
