3f07c43907af300b871e11faba36772b5b88461e
kickbelldev <kickbelldev@gmail.com>
2025-05-13 13:41:20 +0900

    fix(web): update GRAPHQL_ENDPOINT configuration in development and production Dockerfiles

--- apps/web/src/constants/graphql.ts
+++ apps/web/src/constants/graphql.ts
@@ -2,7 +2,7 @@ import { IS_API_MOCKING, IS_PRD } from './env';
 
 export const GRAPHQL_ENDPOINT = IS_API_MOCKING
   ? 'http://localhost:9090/graphql'
-  : (process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ??
+  : (process.env.GRAPHQL_ENDPOINT ??
     (IS_PRD
       ? 'http://crawling-server-lb.crawling-server.svc.cluster.local:3100'
       : 'http://crawling-server-lb.crawling-server-dev.svc.cluster.local:3100'));
