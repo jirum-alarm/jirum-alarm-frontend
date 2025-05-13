1f3aa3453df6f9f14054d52807eb3ce0f9afc314
kickbelldev <kickbelldev@gmail.com>
2025-05-13 12:09:38 +0900

    fix(web): update service URL configuration

--- apps/web/src/constants/env.ts
+++ apps/web/src/constants/env.ts
@@ -1,5 +1,3 @@
 export const IS_API_MOCKING = process.env.NEXT_PUBLIC_API_MOCKING === 'enable';
 export const IS_PRD = process.env.NODE_ENV === 'production';
-export const SERVICE_URL = IS_PRD
-  ? 'https://jirum-alarm.com'
-  : (process.env.NEXT_PUBLIC_SERVICE_URL ?? 'http://dev.jirum-alarm.com');
+export const SERVICE_URL = process.env.NEXT_PUBLIC_SERVICE_URL ?? 'https://dev.jirum-alarm.com';
