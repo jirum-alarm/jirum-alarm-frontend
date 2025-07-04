13793a4eaba0fc413ec8c3b0a1b44c8ea19b198b
tjsry0466 <tjs0466@naver.com>
2025-07-04 09:52:38 +0900

    feat(web): 메타 정보 개선

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata, Viewport } from 'next';
 
 import { AppProvider } from '@/app/(app)/providers';
 import BottomNavServer from '@/components/layout/BottomNavServer';
-import { defaultMetadata } from '@/constants/metadata';
+import { defaultMetadata, jsonLd } from '@/constants/metadata';
 import { pretendard } from '@/lib/fonts';
 
 // const PostHogPageView = dynamic(() => import('@/components/PostHogPageView'), {
@@ -27,6 +27,37 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
         <link rel="dns-prefetch" href="https://api.mixpanel.com" />
         <link rel="preconnect" href="https://cdn.jirum-alarm.com" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="지름알림" />
+        <link rel="preconnect" href="https://cdn.jirum-alarm.com" />
+        <link rel="preconnect" href="https://www.google-analytics.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+        <link rel="preload" as="image" href="/images/icon-144x144.png" />
+        <link
+          rel="preload"
+          as="font"
+          href="/fonts/PretendardStdVariable.woff2"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+
+        <meta name="author" content="지름알림" />
+        <meta name="msapplication-TileImage" content="/images/icon-144x144.png" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+
+        <link
+          rel="search"
+          href="/opensearch.xml"
+          title="지름알림"
+          type="application/opensearchdescription+xml"
+        />
       </head>
       <body>
         <div className="relative min-w-[320px] bg-white before:fixed before:inset-y-0 before:left-1/2 before:z-50 before:w-[1px] before:translate-x-[300px] before:bg-gray-100 after:fixed after:inset-y-0 after:right-1/2 after:z-50 after:w-[1px] after:-translate-x-[300px] after:bg-gray-100">
