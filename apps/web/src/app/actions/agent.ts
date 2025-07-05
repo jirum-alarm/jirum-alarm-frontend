1d9067cd7409857e6b60a4b667cca03dee638ae7
Ssoon-m <tkksm2@gmail.com>
2025-07-05 13:32:11 +0900

    feat: 모바일 웹뷰 userAgent Flutter 대응

--- apps/web/src/app/actions/agent.ts
+++ apps/web/src/app/actions/agent.ts
@@ -7,10 +7,21 @@ async function checkDevice() {
   const userAgentString = ua || '';
 
   const isJirumAlarmIOSApp = Boolean(userAgentString.match(/IOS ReactNative Webview Jirum Alarm/i));
+  const isLegacyJirumAlarmIOSApp = Boolean(
+    userAgentString.match(/IOS Flutter Webview Jirum Alarm/i),
+  );
   const isJirumAlarmAndroidApp = Boolean(
     userAgentString.match(/Android ReactNative Webview Jirum Alarm/i),
   );
-  const isJirumAlarmApp = !!(isJirumAlarmIOSApp || isJirumAlarmAndroidApp);
+  const isLegacyJirumAlarmAndroidApp = Boolean(
+    userAgentString.match(/Android Flutter Webview Jirum Alarm/i),
+  );
+  const isJirumAlarmApp = !!(
+    isJirumAlarmIOSApp ||
+    isJirumAlarmAndroidApp ||
+    isLegacyJirumAlarmIOSApp ||
+    isLegacyJirumAlarmAndroidApp
+  );
   const isMobile = Boolean(device.type === 'mobile') || isJirumAlarmApp;
   const isSafari = Boolean(browser.name === 'Mobile Safari');
 
