69a5709b86e4e4f65c011b2f35c46d7dafb3e723
Jongchan <kickbelldev@gmail.com>
2025-08-10 05:10:19 +0900

    chore: workspace and eslint/next config updates (#264)

--- apps/landing/eslint.config.mjs
+++ apps/landing/eslint.config.mjs
@@ -1,15 +1,10 @@
 import jirum from '@jirum/eslint-config-jirum';
 
-export default [
+const config = [
   ...jirum,
-  {
-    languageOptions: {
-      parserOptions: {
-        project: ['./tsconfig.json'],
-      },
-    },
-  },
   {
     ignores: ['node_modules/', 'dist/', '.next/'],
   },
 ];
+
+export default config;
