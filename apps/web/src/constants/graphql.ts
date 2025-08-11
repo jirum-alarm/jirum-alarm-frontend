9e993d74ef356ddfc2b1876b4c7df21d1dccd513
Jongchan <kickbelldev@gmail.com>
2025-08-11 09:54:25 +0900

    feat(web): 운영환경 GraphQL 요청 k8s 내부통신으로 변경

--- apps/web/src/constants/graphql.ts
+++ apps/web/src/constants/graphql.ts
@@ -6,6 +6,9 @@ export const GRAPHQL_ENDPOINT_PROXY = IS_PRD
 
 export const GRAPHQL_ENDPOINT = IS_API_MOCKING
   ? 'http://localhost:9090/graphql'
-  : process.env.NEXT_PUBLIC_API_URL || 'https://jirum-dev-api.kyojs.com/graphql';
+  : IS_PRD
+    ? process.env.NEXT_PUBLIC_API_URL ||
+      'http://crawling-server-lb.crawling-server.svc.cluster.local:3100/graphql'
+    : 'http://dev-api.jirum-alarm.com/graphql';
 export const GRAPHQL_SUBSCRIPTIONS_ENDPOINT =
   process.env.SUBSCRIPTIONS_GRAPHQL_ENDPOINT || 'wss://jirum-dev-api.kyojs.com/graphql';
