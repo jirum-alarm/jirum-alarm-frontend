6b497f35fd77e34d43d329d0b58a940bbbeebe1d
Jongchan <kickbelldev@gmail.com>
2025-08-11 10:51:51 +0900

    temp(web): GraphQL 개발서버 엔드포인트 추가

--- apps/web/src/constants/graphql.ts
+++ apps/web/src/constants/graphql.ts
@@ -1,8 +1,10 @@
-import { IS_API_MOCKING, IS_PRD, SERVICE_URL } from './env';
+import { DEV_URL, IS_API_MOCKING, IS_PRD, SERVICE_URL } from './env';
 
 export const GRAPHQL_ENDPOINT_PROXY = IS_PRD
   ? SERVICE_URL + '/api/graphql'
-  : 'http://localhost:3000/api/graphql';
+  : DEV_URL + '/api/graphql';
+// 로컬 테스트 시 사용
+// : 'http://localhost:3000/api/graphql';
 
 export const GRAPHQL_ENDPOINT = IS_API_MOCKING
   ? 'http://localhost:9090/graphql'
