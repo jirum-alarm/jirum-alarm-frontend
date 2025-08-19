98b94df396568b6d580f6d39006ad3f0e0f5f5da
Jongchan <kickbelldev@gmail.com>
2025-08-19 12:54:04 +0900

    feat/improve: 상세페이지 prefetch 구조 개선

---
+++ apps/web/src/features/product-detail/prefetch/ProductPrefetch.tsx
@@ -0,0 +1,22 @@
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+import { getQueryClient } from '@/app/(app)/react-query/query-client';
+
+import { ProductQueries } from '@entities/product';
+
+export default async function ProductPrefetch({
+  productId,
+  children,
+}: {
+  productId: number;
+  children: React.ReactNode;
+}) {
+  const queryClient = getQueryClient();
+
+  queryClient.prefetchQuery(ProductQueries.productInfo({ id: productId }));
+  queryClient.prefetchQuery(ProductQueries.productStats({ id: productId }));
+  queryClient.prefetchQuery(ProductQueries.productAdditionalInfo({ id: productId }));
+  queryClient.prefetchQuery(ProductQueries.productGuide({ productId }));
+
+  return <HydrationBoundary state={dehydrate(queryClient)}>{children}</HydrationBoundary>;
+}
--- apps/web/src/features/product-detail/prefetch/index.ts
+++ apps/web/src/features/product-detail/prefetch/index.ts
@@ -1,4 +1,1 @@
-export { default as ProductInfoPrefetch } from './ProductInfoPrefetch';
-export { default as ProductStatsPrefetch } from './ProductStatsPrefetch';
-export { default as ProductAdditionalInfoPrefetch } from './ProductAdditionalInfoPrefetch';
-export { default as ProductGuidesPrefetch } from './ProductGuidesPrefetch';
+export { default as ProductPrefetch } from './ProductPrefetch';
