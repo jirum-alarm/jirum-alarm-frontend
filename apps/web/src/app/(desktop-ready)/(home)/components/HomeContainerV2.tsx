0f94ea2d7f61cb67183e9ba5236881d03a2c2351
Jongchan <kickbelldev@gmail.com>
2025-08-19 12:40:05 +0900

    feat(web): 홈 배너 스와이퍼 개선

--- apps/web/src/app/(desktop-ready)/(home)/components/HomeContainerV2.tsx
+++ apps/web/src/app/(desktop-ready)/(home)/components/HomeContainerV2.tsx
@@ -1,12 +1,16 @@
-import DeviceSpecific from '@/components/layout/DeviceSpecific';
+import { checkDevice } from '@/app/actions/agent';
 
 import { LiveHotDealSection } from '@widgets/live-hotdeal';
 import { RecommendedProductSection } from '@widgets/recommend';
 
 import DesktopHeroSection from '../desktop/HeroSection';
+import MobileBackgroundHeader from '../mobile/BackgroundHeader';
+import MobileHomeHeader from '../mobile/HomeHeader';
 import MobileJirumRankingContainer from '../mobile/JirumRankingContainer';
 
-const HomeContainerV2 = () => {
+async function HomeContainerV2() {
+  const { isMobile } = await checkDevice();
+
   const renderDesktop = () => {
     return (
       <>
@@ -16,6 +20,17 @@ const HomeContainerV2 = () => {
   };
 
   const renderMobile = () => {
+    {
+      return (
+        <>
+          <MobileHomeHeader />
+          <MobileBackgroundHeader />
+        </>
+      );
+    }
+  };
+
+  const renderMobileRanking = () => {
     return (
       <>
         <MobileJirumRankingContainer />
@@ -26,10 +41,10 @@ const HomeContainerV2 = () => {
 
   return (
     <div className="pc:max-w-none max-w-mobile-max mx-auto h-full w-full pb-[70px]">
-      <DeviceSpecific desktop={renderDesktop} />
-      <main className="pc:mt-[770px] pc:w-full pc:max-w-none pc:rounded-t-[1.75rem] pc:pt-[72px] max-w-mobile-max relative z-10 mx-auto mt-[153px] h-full rounded-t-[1.25rem] bg-white pt-3">
+      {!isMobile ? renderDesktop() : renderMobile()}
+      <main className="pc:mt-[770px] pc:w-full pc:max-w-none pc:rounded-t-[1.75rem] pc:pt-[72px] max-w-mobile-max relative z-10 mx-auto mt-[158px] h-full rounded-t-[1.25rem] bg-white pt-3">
         <div className="pc:mx-auto pc:max-w-layout-max">
-          <DeviceSpecific mobile={renderMobile} />
+          {!isMobile ? null : renderMobileRanking()}
           <div className="pc:gap-y-15 pc:pt-0 flex flex-col gap-y-8 py-3">
             <RecommendedProductSection />
             <LiveHotDealSection />
@@ -38,6 +53,6 @@ const HomeContainerV2 = () => {
       </main>
     </div>
   );
-};
+}
 
 export default HomeContainerV2;
--- apps/web/src/app/(desktop-ready)/(home)/mobile/BackgroundHeader.tsx
+++ apps/web/src/app/(desktop-ready)/(home)/mobile/BackgroundHeader.tsx
@@ -3,25 +3,19 @@ import { IconLogo } from '@/components/common/icons/Illust';
 import SearchLinkButton from '@/components/SearchLinkButton';
 import { cn } from '@/lib/cn';
 
-import AboutLink from '@features/banner/items/AboutLink';
-import AppDownloadCTA from '@features/banner/items/AppDownloadCTA';
-import KakaoOpenChatLink from '@features/banner/items/KakaoOpenChatLink';
-
 import BannerSwiper from './BannerSwiper';
 
 const BackgroundHeader = async () => {
-  const { isSafari, isMobile, isJirumAlarmApp } = await checkDevice();
-
-  const isAppDownloadVisible = isMobile && !isJirumAlarmApp;
+  const device = await checkDevice();
 
   return (
     <div
       className={cn(
         'max-w-mobile-max fixed top-0 z-0 mx-auto h-[300px] w-full bg-gray-900',
-        isSafari && 'top-[0.5px]',
+        device.isSafari && 'top-[0.5px]',
       )}
     >
-      <div className="max-w-mobile-max mx-auto">
+      <div className="max-w-mobile-max mx-auto w-full">
         <header className="flex h-14 w-full items-center justify-between px-5 py-3">
           <div className="flex items-center gap-2">
             <IconLogo />
@@ -29,14 +23,7 @@ const BackgroundHeader = async () => {
           </div>
           <SearchLinkButton color="#FFF" />
         </header>
-        <BannerSwiper>
-          {isAppDownloadVisible && <AppDownloadCTA />}
-          <KakaoOpenChatLink />
-          <AboutLink />
-          {isAppDownloadVisible && <AppDownloadCTA />}
-          <KakaoOpenChatLink />
-          <AboutLink />
-        </BannerSwiper>
+        <BannerSwiper device={device} />
       </div>
     </div>
   );
