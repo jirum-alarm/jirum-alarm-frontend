7160ddd248247bec45c8e464b083b4671c05de25
Jongchan <kickbelldev@gmail.com>
2025-08-26 22:39:33 +0900

    fix: 하이드레이션 경고 예상부분 suppressHydrationWarning 추가

--- apps/web/src/app/(desktop-ready)/products/[id]/components/desktop/ViewerCount.tsx
+++ apps/web/src/app/(desktop-ready)/products/[id]/components/desktop/ViewerCount.tsx
@@ -3,7 +3,7 @@ export default function ViewerCount({ count }: { count: number }) {
     <div className="bg-secondary-50 flex h-[48px] w-full items-center justify-center whitespace-nowrap">
       <span className="text-sm text-gray-700">
         지금&nbsp;
-        <strong className="text-secondary-500 font-semibold">
+        <strong className="text-secondary-500 font-semibold" suppressHydrationWarning>
           {count.toLocaleString('ko-kr')}명
         </strong>
         이 보고 있어요
--- apps/web/src/shared/ui/DisplayTime.tsx
+++ apps/web/src/shared/ui/DisplayTime.tsx
@@ -1,10 +1,7 @@
 'use client';
 
-import { useIsHydrated } from '@/hooks/useIsHydrated';
 import { displayTime } from '@/util/displayTime';
 
 export default function DisplayTime({ time }: { time: Date }) {
-  const isHydrated = useIsHydrated();
-
-  return <span suppressHydrationWarning>{isHydrated ? displayTime(time) : ''}</span>;
+  return <span suppressHydrationWarning>{displayTime(time)}</span>;
 }
