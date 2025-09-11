321421d2e61854ec58144695f6d1b8e22baf132c
tjsry0466 <tjs0466@naver.com>
2025-09-12 08:30:46 +0900

    feat: 쿠팡 파트너스 문구 변경

--- apps/web/src/app/(desktop-ready)/products/[id]/components/CoupangPartnerGuide.tsx
+++ apps/web/src/app/(desktop-ready)/products/[id]/components/CoupangPartnerGuide.tsx
@@ -17,7 +17,7 @@ export default function CoupangPartnerGuide({ productId }: { productId: number }
     <div className="pc:rounded-xl flex h-12 items-center justify-start gap-3 self-stretch bg-gray-100 px-3.5 py-3.5">
       <div className="justify-start text-xs leading-none font-medium text-slate-700">안내</div>
       <div className="flex-1 justify-start text-xs leading-none font-normal text-slate-600">
-        쿠팡 파트너스 활동으로 지름알림에 일정 금액의 수수료가 지급될 수 있습니다.
+        쿠팡 파트너스 활동으로 지름알림이 일정 금액의 수수료를 지급 받습니다.
       </div>
     </div>
   );
