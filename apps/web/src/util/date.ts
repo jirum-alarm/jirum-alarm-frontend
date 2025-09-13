55c7a7784fc232da0d8e0f464f2228a9f9e19640
Ssoon-m <tkksm2@gmail.com>
2025-09-14 01:36:55 +0900

    feat: 현재 날짜가 시작날짜,종료날짜 범위 안에 존재하는지 판단하는 date util 함수 추가

--- apps/web/src/util/date.ts
+++ apps/web/src/util/date.ts
@@ -1,9 +1,15 @@
-import dayjs from 'dayjs';
+import dayjs, { DayjsTimezone } from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import 'dayjs/locale/ko';
+
+export type TimeZone = 'Asia/Seoul' | 'America/New_York';
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime);
+dayjs.extend(timezone);
+dayjs.extend(utc);
 
 export const getDayBefore = (type: number) => {
   return dayjs().add(-type, 'day').startOf('minute').toDate();
@@ -19,3 +25,29 @@ export const getFromNow = (date: string) => {
 
   return dayjsDate.fromNow();
 };
+
+export const period = (timeZone: TimeZone = 'Asia/Seoul') => ({
+  start: (startDate: string) => {
+    return {
+      end: (endDate: string) => {
+        const now = dayjs().tz(timeZone);
+
+        if (startDate) {
+          const start = dayjs(startDate).tz(timeZone);
+          if (now.isBefore(start)) {
+            return false;
+          }
+        }
+
+        if (endDate) {
+          const end = dayjs(endDate).tz(timeZone).endOf('day');
+          if (now.isAfter(end)) {
+            return false;
+          }
+        }
+
+        return true;
+      },
+    };
+  },
+});
